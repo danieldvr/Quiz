e35e160bac3bb374e2732d8fbc332ef106fe8928
danieldvr <daniell.vieira.rodrigues@gmail.com>
2024-07-07 19:23:35 -0300

    first commit

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,41 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+            "1. Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?",
+            "2. De quem é a famosa frase -Penso, logo existo-?",
+            "3. De onde é a invenção do chuveiro elétrico?",
+            "4. Quais o menor e o maior país do mundo?",
+            "5. Qual o nome do presidente do Brasil que ficou conhecido como Jango?",
+            "6. Qual o grupo em que todas as palavras foram escritas corretamente?",
+            "7. Qual o livro mais vendido no mundo a seguir à Bíblia?",
+            "8. Quantas casas decimais tem o número pi?",
+            "9. Atualmente, quantos elementos químicos a tabela periódica possui?",
+            "10. Quais os países que têm a maior e a menor expectativa de vida do mundo?",
+            "11. O que a palavra legend significa em português?",
+            "12. Qual o número mínimo de jogadores numa partida de futebol?",
+            "13. Quais os principais autores do Barroco no Brasil?",
+            "14. Quais as duas datas que são comemoradas em novembro?",
+            "15. Quem pintou \"Guernica\"?"
+    };
+
+    public static String choices[][] = {
+            {"Tem entre 2 a 4 litros. São retirados 450 mililitros", "Tem entre 4 a 6 litros. São retirados 450 mililitros", "Tem 10 litros. São retirados 2 litros", "Tem 7 litros. São retirados 1,5 litros"},
+            {"Platão", "Galileu Galilei", "Descartes", "Sócrates"},
+            {"França", "Inglaterra", "Brasil", "Austrália"},
+            {"Vaticano e Rússia", "Nauru e China", "Mônaco e Canadá", "Malta e Estados Unidos"},
+            {"Jacinto Anjos", "Getúlio Vargas", "João Figueiredo", "João Goulart"},
+            {"Asterístico, beneficiente, meteorologia, entertido", "Asterisco, beneficente, meteorologia, entretido", "Asterisco, beneficente, metereologia, entretido", "Asterístico, beneficiente, metereologia, entretido"},
+    };
+
+    public static String correctAnswers[] = {
+            "Tem entre 4 a 6 litros. São retirados 450 mililitros",
+            "Descartes",
+            "Brasil",
+            "Vaticano e Rússia",
+            "João Goulart",
+            "Asterisco, beneficente, meteorologia, entretido",
+    };
+
+}
